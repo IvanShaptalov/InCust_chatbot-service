4f3eba87c0e308e88ee81e6cfc2515260a0a29a1
IvanShaptalov <qwr111792@gmail.com>
2021-10-26 21:45:20 +0300

    fixed: sending image in low quality

--- utils/messenger.py
+++ utils/messenger.py
@@ -122,7 +122,7 @@ class PhotoSender(BaseSender):
     async def forward_data(self, data: types.Message):
         user_in_service_bot = False
         path = os.path.join(config.media_path, 'tmp_photo')
-        photo = await data.photo[0].download(destination_file=path)
+        photo = await data.photo[-1].download(destination_file=path)
         with client_bot.with_token(config.CLIENT_BOT_TOKEN):
             chat_id = self.sender_id
 
